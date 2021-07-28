61d77359092a6aadb875de3c5c9efe329b77367b
Rachel Lima <rachellimaa@gmail.com>
2021-07-28 17:40:30 -0300

    Add composite disposable

--- app/src/main/java/com/anushka/rxdemo1/MainActivity.java
+++ app/src/main/java/com/anushka/rxdemo1/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.observers.DisposableObserver;
 import io.reactivex.rxjava3.schedulers.Schedulers;
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Observable<String> myObservable;
     private DisposableObserver<String> myObserver;
+    private DisposableObserver<String> myObserver2;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     private TextView textView;
     @Override
@@ -28,13 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=findViewById(R.id.tvGreeting);
-
         myObservable = Observable.just(greeting);
-
-        myObservable.subscribeOn(Schedulers.io());
-
-
-        myObservable.observeOn(AndroidSchedulers.mainThread());
 
         myObserver = new DisposableObserver<String>() {
             @Override
@@ -54,7 +52,32 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        myObservable.subscribe(myObserver);
+        myObserver2 = new DisposableObserver<String>() {
+            @Override
+            public void onNext(@NonNull String s) {
+                Log.i("TAG", "onNext 2 ");
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.i("TAG", "onError 2");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i("TAG", "onComplete 2 ");
+            }
+        };
+
+
+        compositeDisposable.add(myObservable
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeWith(myObserver));
+
+        compositeDisposable.add(myObservable
+                .subscribeWith(myObserver2));
 
 
     }
@@ -62,6 +85,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        myObserver.dispose();
+        compositeDisposable.clear();
     }
 }
