76c17b1515f102c40bb262e112b76cf9e70e1a81
Rachel Lima <rachellimaa@gmail.com>
2021-07-29 17:52:27 -0300

    Add just operator
    Signed-off-by: Rachel Lima <rachellimaa@gmail.com>

--- app/src/main/java/com/anushka/rxdemo1/MainActivity.java
+++ app/src/main/java/com/anushka/rxdemo1/MainActivity.java
@@ -1,44 +1,50 @@
 package com.anushka.rxdemo1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
-import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.observers.DisposableObserver;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 
 public class MainActivity extends AppCompatActivity {
-    private final static String TAG="myApp";
-    private String greeting="Hello From RxJava";
+    private final static String TAG = "myApp";
+    private String[] greeting = {"Hello A", "Hello B", "Hello C"};
 
-    private Observable<String> myObservable;
-    private DisposableObserver<String> myObserver;
-    private DisposableObserver<String> myObserver2;
+    private Observable<String[]> myObservable;
+    private DisposableObserver<String[]> myObserver;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.tvGreeting);
+        textView = findViewById(R.id.tvGreeting);
+
+        // Or works with strings like this:  "Hello A", "Hello B", "Hello C"
         myObservable = Observable.just(greeting);
 
-        myObserver = new DisposableObserver<String>() {
+        compositeDisposable.add(myObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(getObserver()));
+
+    }
+
+    private DisposableObserver getObserver() {
+        myObserver = new DisposableObserver<String[]>() {
             @Override
-            public void onNext(@NonNull String s) {
-                Log.i("TAG", "onNext");
-                textView.setText(greeting);
+            public void onNext(@NonNull String[] s) {
+                Log.i("TAG", "onNext " + s);
             }
 
             @Override
@@ -52,34 +58,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        myObserver2 = new DisposableObserver<String>() {
-            @Override
-            public void onNext(@NonNull String s) {
-                Log.i("TAG", "onNext 2 ");
-                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-                Log.i("TAG", "onError 2");
-            }
-
-            @Override
-            public void onComplete() {
-                Log.i("TAG", "onComplete 2 ");
-            }
-        };
-
-
-        compositeDisposable.add(myObservable
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribeWith(myObserver));
-
-        compositeDisposable.add(myObservable
-                .subscribeWith(myObserver2));
-
-
+        return myObserver;
     }
 
     @Override
